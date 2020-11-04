9f6e5e1f4b1bf183f1fe910543cbdab7973af2e6
katsu-piko <katsudon0064@gmail.com>
2020-11-04 11:54:46 +0900

    新規登録/ログイン/ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  validates :password,        format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :nickname,        presence: true
+  validates :last_name,       presence: true,format: { with: /\A(?=.*?[ぁ-んァ-ン一-龥])[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name,      presence: true,format: { with: /\A(?=.*?[ぁ-んァ-ン一-龥])[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_name_kana,  presence: true,format: { with: /\A(?=.*?[ァ-ンー－]+$)[ァ-ンー－]+$+\z/ }
+  validates :first_name_kana, presence: true,format: { with: /\A(?=.*?[ァ-ンー－]+$)[ァ-ンー－]+$+\z/ }
+  validates :birthday,        presence: true
+
 end
