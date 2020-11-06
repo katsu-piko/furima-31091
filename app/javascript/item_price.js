776bf31d738c41eb309cf24521f5bbed52cf6da0
katsu-piko <katsudon0064@gmail.com>
2020-11-06 19:56:43 +0900

    販売手数料と販売利益の計算

---
+++ app/javascript/item_price.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = inputValue / 10
+    const profitDom = document.getElementById("profit");
+    profitDom.innerHTML = inputValue - addTaxDom.innerHTML
+
+  });
+
+});
