65db3f012b255fa651b8be2c30fe4ebccedd739a
katsu-piko <katsudon0064@gmail.com>
2020-11-06 12:23:28 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shopping_fee_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
+
   belongs_to :user
-  has_one_attached :item
+  has_one_attached :image
 end
