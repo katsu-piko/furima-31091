2bc5546b1d937f71eaed26924ae34bdf7733c161
katsu-piko <katsudon0064@gmail.com>
2020-11-06 14:23:05 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,15 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :sales_status_id, numericality: { other_than: 1 } 
+  validates :shopping_fee_status_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :scheduled_delivery_id, numericality: { other_than: 1 } 
+  validates :price, presence: true, format: { with: /\A(?=.*?[0-9]+$)[0-9]+$+\z/ }, numericality: { greater_than_or_equal_to: 300 }{ less_than_or_equal_to: 9999999 }
+
 end
