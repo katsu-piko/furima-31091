aa527ad86e9878ca47035e29b9e02c21f0e4c4e9
katsu-piko <katsudon0064@gmail.com>
2020-11-11 14:30:14 +0900

    編集ページへの遷移設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,13 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shopping_fee_status_id,
