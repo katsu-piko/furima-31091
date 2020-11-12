553ccf1b208c1640a5796b06d1fde705b60db924
katsu-piko <katsudon0064@gmail.com>
2020-11-12 16:48:51 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items
+  has_many :item_purchases
 
   validates :password,        format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   with_options presence: true do
