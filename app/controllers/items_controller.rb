b851d83e2ea8c7472e73c919746d74ef4cba5e57
katsu-piko <katsudon0064@gmail.com>
2020-11-16 16:56:16 +0900

    編集ページへのアクセス制限の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,7 +22,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to action: :index unless current_user.id == @item.user_id
+    redirect_to root_path if current_user.id != @item.user_id || ItemPurchase.exists?(item_id: @item)
   end
 
   def update
